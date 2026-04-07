669484dd592375d598e448c82b7e48f396903671
good_ruff2436 <itschool.ikeyama@gmail.com>
2026-04-07 14:13:38 +0900

    Enhance bubble sort with output and comments
    Added print statement for sorted data and provided bubble sort algorithm in comments.

--- it-passport/02_bubble_sort.py
+++ it-passport/02_bubble_sort.py
@@ -9,3 +9,17 @@ data = [5, 3, 8, 4, 2]
                 data[j-1], data[j] = data[j], data[j-1]
 
     print(f"整列後：{data}")
+
+# バブルソート 令和5年問60
+
+○printArray()
+整数型：n, m
+整数型の配列：integerArray() ← {2, 4, 1, 3}
+for(nを1から(integerArrayの要素数－1)まで1ずつ増やす)
+　for(mを1から(integerArrayの要素数－1)まで1ずつ増やす)
+　　if(integerArray[m] > integerArray[m+1])
+　　　integerArray[m]とintegerArray[m+1]の値を入れ替える
+　　endif
+　endfor
+endfor
+integerArrayの全ての要素を先頭から順にコンマ区切りで出力する
